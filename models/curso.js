4ee2ece2f7072e7424e73e8b9c4bdb0ff67b1595
Orlando <xlandoxrojasx@gmail.com>
2020-07-09 10:19:46 -0500

    Se agrego el modelo de cursos.

---
+++ models/curso.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const cursoSchema = new Schema({
+    nom: String,
+    des: String,
+    exp_id: {type: Array, default: []},
+    f_ini: Date,
+    f_fin: Date,
+    cupo: Number,
+    usu_id: {type: Array, default: []},
+    f_crea: {type: Date, default: Date.now},
+    activo: {type: Boolean, default: true}
+});
+
+// Convertir a modelo
+const Curso = mongoose.model('Curso', cursoSchema);
+
+export default Curso;
